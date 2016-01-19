b7f6a5451aedf1f45ce8d42b3d326f08ecbd74fe
snoopydesbois <tbenoist@BE11-10.ens-sfa.univ-poitiers.fr>
2016-01-19 14:15:28 +0100

    Début création classes 2D

--- ApplicationDiscret/Application/Js/Objects/Curve.js
+++ ApplicationDiscret/Application/Js/Objects/Curve.js
@@ -1,7 +1,76 @@
 Curve.prototype.constructor = Curve;
 
-function Curve(){}
+function Curve(){
+	
+}
 
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.setActive = fucntion(nom){
+	
+}
+
+
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.getPoints = function(dim){
+
+}
+
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.startfreehand = function(dim){
+}
+
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.newFreeHand = function(dim, coord){
+}
+
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.addPoint = function(coord, dim){
+}
+
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.drawLastSegment = function(){
+}
+
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.parseImplicit = function(eq){
+}
+
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.parseParametric = function(eqX, eqY){
+}
+
+//==============================================================================
+/**
+*
+*/
+Curve.prototype.parseExplicit = function(eq){
+}
+
+//==============================================================================
 Curve.prototype.computePoints = function(){
 	throw "Curve.computePoints.ErrorNotImplementedInAbstractClass";
 };
