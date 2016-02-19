a7c77300478f7630e0a97ba0cd236bff5a1a2c72
Lydie Richaume <Lydie Richaume>
2016-02-19 14:34:39 +0100

    Amelioration des paramètres de la droite

--- ApplicationDiscret/Application/Curves/Revolution/Circle.js
+++ ApplicationDiscret/Application/Curves/Revolution/Circle.js
@@ -7,12 +7,9 @@ Circle.prototype.constructor = Circle;
 * @constructor {Equation} the equation of the curve
 */
 function Circle() {
-//	var equation = new Equation("a*(x - h)^2 + b*(y - k)^2 - r^2");
-	var equation = new Equation("x^2 + (y * b)^2 - 1");
-	// equation.setParameter("a", 1);
-	equation.setParameter("b", 1);
-	// equation.setParameter("radius", 1);
+	var equation = new Equation("x^2 + (y * ecrasement)^2 - 1");
+	equation.setParameter("ecrasement", 1);
 	ImplicitCurve.call(this, equation);
 	
-	this.parametersRange['b'] = new Range(1.0,5.0);
+	this.parametersRange['ecrasement'] = new Range(1.0,5.0);
 };
