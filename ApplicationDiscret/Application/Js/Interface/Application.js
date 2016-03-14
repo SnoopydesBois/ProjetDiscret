daf2214d0d03c4d8ecb8f4643f4511ffebd02e50
Adrien Bisutti <adrien.bisutti@etu.univ-poitiers.fr>
2016-03-14 10:49:59 +0100

    COrrection d'erreur.

--- ApplicationDiscret/Application/Js/Interface/Application.js
+++ ApplicationDiscret/Application/Js/Interface/Application.js
@@ -72,53 +72,6 @@ Application.prototype.constructor = Application;
  */
 function Application () {
 
-		// Licence
-
-
-	console.log ('%c©2015 : BENOIST Thomas, BISUTTI Adrien, DESPLEBAIN Tanguy, '
-	+'LAURET Karl\n'
-	+'%c\n'
-	+'benoist.thomas@hotmail.fr\n'
-	+'biscui_86@hotmail.fr\n'
-	+'tanguy.desplebain@gmail.com\n'
-	+'lauret.karl@hotmail.fr\n'
-	+'%c\n'
-	+'Ce logiciel est un programme informatique servant à modéliser des '
-	+'structures 3D voxellisées.\n'
-	+'\n'
-	+'Ce logiciel est régi par la licence CeCILL soumise au droit français et '
-	+'respectant les principes de diffusion des logiciels libres.\nVous pouvez '
-	+'utiliser, modifier et/ou redistribuer ce programme sous les conditions '
-	+'de la licence CeCILL telle que diffusée par le CEA, le CNRS et l\'INRIA '
-	+'sur le site %c"http://www.cecill.info"%c.\n'
-	+'\n'
-	+'En contrepartie de l\'accessibilité au code source et des droits de copie'
-	+', de modification et de redistribution accordés par cette licence, il n\''
-	+'est offert aux utilisateurs qu\'une garantie limitée.\nPour les mêmes '
-	+'raisons, seule une responsabilité restreinte pèse sur l\'auteur du '
-	+'programme, le titulaire des droits patrimoniaux et les concédants '
-	+'successifs.\n\n'
-	+'A cet égard  l\'attention de l\'utilisateur est attirée sur les risques'
-	+'associés au chargement, à l\'utilisation, à la modification et/ou au'
-	+'développement et à la reproduction du logiciel par l\'utilisateur étant\n'
-	+'donné sa spécificité de logiciel libre, qui peut le rendre complexe à'
-	+'manipuler et qui le réserve donc à des développeurs et des professionnels'
-	+'avertis possédant  des  connaissances  informatiques approfondies.\nLes'
-	+'utilisateurs sont donc invités à charger  et  tester  l\'adéquation  du'
-	+'logiciel à leurs besoins dans des conditions permettant d\'assurer la'
-	+'sécurité de leurs systèmes et ou de leurs données et, plus généralement,'
-	+'\nà l\'utiliser et l\'exploiter dans les mêmes conditions de sécurité.\n'
-	+'\n'
-	+'Le fait que vous puissiez accéder à cet en-tête signifie que vous avez'
-	+'pris connaissance de la licence CeCILL, et que vous en avez accepté les'
-	+'termes\n'
-	+'\n***********************************************************************'
-	+'***********************************************************************\n'
-	, 'color: #FF0000; font-weight:bold',
-	'color: #FF0000; text-decoration: underline', 'color: #FF0000',
-	'color: #FF0000; text-decoration: underline', 'color: #FF0000');
-
-
 
 		  ///////////////
 		 /// Surface ///
